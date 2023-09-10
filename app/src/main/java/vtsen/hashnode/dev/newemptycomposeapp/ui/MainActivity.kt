bfa9cc2570c52b67f889d0ebd8a6d49d634837a0
vinchamp77 <vinchamp77@gmail.com>
2023-09-10 12:45:54 +1000

    Add by viewModels example

--- app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/MainActivity.kt
+++ app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/MainActivity.kt
@@ -18,16 +18,36 @@ package vtsen.hashnode.dev.newemptycomposeapp.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import vtsen.hashnode.dev.newemptycomposeapp.repository.ARepository
+import vtsen.hashnode.dev.newemptycomposeapp.repository.BRepository
 import vtsen.hashnode.dev.newemptycomposeapp.ui.screens.MainScreen
 import vtsen.hashnode.dev.newemptycomposeapp.ui.theme.ViewModelFactoryExampleAppTheme
+import vtsen.hashnode.dev.newemptycomposeapp.ui.viewmodel.AViewModel
+import vtsen.hashnode.dev.newemptycomposeapp.ui.viewmodel.AViewModelFactory
+import vtsen.hashnode.dev.newemptycomposeapp.ui.viewmodel.BViewModel
+import vtsen.hashnode.dev.newemptycomposeapp.ui.viewmodel.createViewModelFactory
 
 class MainActivity : ComponentActivity() {
+
+    // without helper function
+    private val aViewModel by viewModels<AViewModel> {
+        AViewModelFactory(ARepository())
+    }
+
+    // with helper function
+    private val bViewModel by viewModels<BViewModel> {
+        createViewModelFactory {
+            BViewModel(BRepository())
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setupSplashScreen()
 
